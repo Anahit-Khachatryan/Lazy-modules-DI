40efa44de692d62289b39ef23127489e1942f581
VOLO-DC\anahit.khachatryan <anahit.khachatryan@volo.global>
2024-09-23 17:49:57 +0400

    bootstrapApplication, browserPlatform

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
+import { HelpsService } from './app/helps.service';
 
 bootstrapApplication(AppComponent, appConfig)
+// .then(appRef => console.log(98,appRef.injector.get(HelpsService)))
   .catch((err) => console.error(err));
+  
+  //bootstrapApplication sa veradardznum e ApplicationRef vor karank orinak urish tegh inject anenk
+  // platformBrowserDynamic - modulneri hamar e hima vor standalone e da petk chi parzecvel e. sra jamanak sra het steghtsvum e platform Injectory
+  //ete grum enk providedIn: 'platform' platformin ev platformov bootstrap eghats bolor injectornerin
+  // platfrom core-ic jarangum e core dynamicy, dranic el browser dynamicy
+  
+  //DomSanitizer security-i harcy lutsum e
